75d44f309a5a954fa66d25f63de36ae672e73da2
Paul Cushman <pcushman@wickr.com>
2020-03-15 07:24:17 -0700

    Started to remove the token specific references from the configure.js file.  Added the use of the .env.configure file for environment variables for the configuration process.

--- configure.js
+++ configure.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
+const util = require('util')
+
+require("dotenv").config({
+  path: `.env.configure`
+})
+
 const prompt = require('prompt');
 const processes = require('./processes.json');
 const dataStringify = JSON.stringify(processes);
 const dataParsed = JSON.parse(dataStringify);
 const {exec, execSync, execFileSync} = require('child_process');
 //Add any tokens(as strings separated by commas) you want to prompt for in the configuration process here
-const tokens = ['WICKRIO_BOT_NAME', 'DATABASE_ENCRYPTION_KEY', 'WHITELISTED_USERS'];
+const tokens = ['WICKRIO_BOT_NAME', 'DATABASE_ENCRYPTION_KEY', 'ADMINISTRATORS', 'VERIFY_USERS'];
 
 prompt.colors = false;
 
@@ -42,7 +48,8 @@ process.on('uncaughtException', exitHandler.bind(null, {
 
 main();
 
-async function main() {
+async function main()
+{
   if (processConfigured()) {
     try {
       var cp = execSync('cp processes.json processes_backup.json');
@@ -78,43 +85,50 @@ async function inputTokens() {
   return new Promise((resolve, reject) => {
     var recursivePrompt = function() {
       var token = tokens[i];
-      var type;
       if (i === tokens.length) {
         return resolve("Configuration complete!");
       }
-      //If added another config value name to the tokens array in line 52,
-      //you would need to add another if statement block below with the respectful config value name
-      if (token === 'WICKRIO_BOT_NAME' && process.env.WICKRIO_BOT_NAME !== undefined) {
-        var input = token + '=' + process.env.WICKRIO_BOT_NAME;
-        config.push(input);
-        i++;
-        return recursivePrompt();
-      } else if (token === 'DATABASE_ENCRYPTION_KEY' && process.env.DATABASE_ENCRYPTION_KEY !== undefined) {
-        var input = token + '=' + process.env.DATABASE_ENCRYPTION_KEY;
-        config.push(input);
-        i++;
-        return recursivePrompt();
-      } else if (token === 'WHITELISTED_USERS' && process.env.WHITELISTED_USERS !== undefined) {
-        var input = token + '=' + process.env.WHITELISTED_USERS;
+
+      // For this token if it is defined in the environment
+      // Then set the input value for the token
+      if (process.env[token] !== undefined) {
+        var input = token + '=' + process.env[token];
         config.push(input);
         i++;
         return recursivePrompt();
       }
+
       var dflt = newObjectResult[token];
       var emptyChoice = false;
-      if (dflt === "undefined" || dflt === undefined) {
-        dflt = "N/A";
-        emptyChoice = true;
+      var patternType;
+      var messageText;
+
+      // If this is a VERITY_USERS then set the type, all others are strings
+      if (token === 'VERIFY_USERS') {
+        messageText = 'Please enter either manual or automatic';
+        typeValue = 'string';
+        patternType = 'manual|automatic';
+        if (dflt === "undefined" || dflt === undefined) {
+          dflt = "automatic";
+        }
+      } else {
+        messageText = 'Cannot leave ' + token + ' empty! Please enter a value';
+        typeValue = 'string';
+        if (dflt === "undefined" || dflt === undefined) {
+          dflt = "N/A";
+          emptyChoice = true;
+        }
       }
+
       var schema = {
         properties: {
           [token]: {
-            pattern: type,
-            type: 'string',
+            pattern: patternType,
+            type: typeValue,
             description: 'Enter your ' + token.replace(/_/g, " ").replace(/\w\S*/g, function(txt) {
               return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             }) + ' (Default: ' + dflt + ')',
-            message: 'Cannot leave ' + token + ' empty! Please enter a value',
+            message: messageText,
             required: emptyChoice
           }
         }
@@ -160,14 +174,22 @@ async function inputTokens() {
         };
         newObjectResult.DATABASE_ENCRYPTION_KEY = obj;
         continue;
-      } else if (key === 'WHITELISTED_USERS' && process.env.WHITELISTED_USERS !== undefined) {
+      } else if (key === 'ADMINISTRATORS' && process.env.ADMINISTRATORS !== undefined) {
         var obj = {
-          "value": process.env.WHITELISTED_USERS,
+          "value": process.env.ADMINISTRATORS,
           "encrypted": false
         };
-        newObjectResult.WHITELISTED_USERS = obj;
+        newObjectResult.ADMINISTRATORS = obj;
+        continue;
+      } else if (key === 'VERIFY_USERS' && process.env.VERIFY_USERS !== undefined) {
+        var obj = {
+          "value": process.env.VERIFY_USERS,
+          "encrypted": false
+        };
+        newObjectResult.VERIFY_USERS = obj;
         continue;
       }
+
       var obj = {
         "value": newObjectResult[key],
         "encrypted": false
@@ -223,19 +245,19 @@ function getCurrentValues()
         return newObjectResult;
     }
 
-    if (pjson.apps[0].env.tokens.WICKRIO_BOT_NAME !== undefined) {
-      newObjectResult['WICKRIO_BOT_NAME'] = pjson.apps[0].env.tokens.WICKRIO_BOT_NAME.value;
-    }
-    if (pjson.apps[0].env.tokens.DATABASE_ENCRYPTION_KEY !== undefined) {
-      newObjectResult['DATABASE_ENCRYPTION_KEY'] = pjson.apps[0].env.tokens.DATABASE_ENCRYPTION_KEY.value;
-    }
-    if (pjson.apps[0].env.tokens.WHITELISTED_USERS !== undefined) {
-      newObjectResult['WHITELISTED_USERS'] = pjson.apps[0].env.tokens.WHITELISTED_USERS.value;
+    // Create a mapping of the list of tokens and their values
+    for(var attributename in pjson.apps[0].env.tokens){
+        newObjectResult[attributename] = pjson.apps[0].env.tokens[attributename].value;
     }
 
     return newObjectResult;
 }
 
+/*
+ * This function will check if any of the tokens have NOT been configured.
+ * If all tokens have values assigned then a true value is returned.
+ * If any tokens do not have values assigned then a false value is returned.
+ */
 function processConfigured()
 {
     var processes;
@@ -256,11 +278,14 @@ function processConfigured()
         return false;
     }
 
-    if (pjson.apps[0].env.tokens.WICKRIO_BOT_NAME === undefined) {
-        return false;
+    // Check if the value for any of the tokens is not set
+    // If it is not set then return false
+    for (var i = 0; i < tokens.length; i++) {
+        if (pjson.apps[0].env.tokens[tokens[i]] === undefined) {
+            return false;
+        }
     }
-    if (pjson.apps[0].env.tokens.WHITELISTED_USERS === undefined) {
-        return false;
-    }
+
+    // All of the tokens have values set
     return true;
 }
--- configure.sh
+++ configure.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 #
+# Remove the configure environment variable file
+#
+rm -f .env.configure
+
+#
 # If the input argument exists then check if it is a file
 # if so it should contain a list of key=value entries.
 #
 if [ -n "$1" ]; then
   if [ -f "$1" ]; then
     . "$1"
+    cp "$1" .env.configure
   fi
 fi
 
