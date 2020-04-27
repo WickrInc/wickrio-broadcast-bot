fe2f0b154435721ce63cd51a44377bdf66b8a156
pwcushman <pcushman@wickr.com>
2020-04-27 10:48:21 -0400

    Paul web (#14)
    * Adding web-interface to control the broadcast bot
    
    * Check if the user that is requesting the authentication code
    
    * save changes to the get list web api
    
    * Added message status web REST apis
    
    * Adding configure that will allows for embedded config values
    
    * support checking if files exists in configuration results
    
    * fix the version number of the bot-api
    
    * Fix some issues with configuration
    
    * Remove an error/warning about the deprecated Buffer function

--- broadcast-bot.js
+++ broadcast-bot.js
@@ -1,6 +1,13 @@
 'use strict';
 
 const fs = require('fs');
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const helmet = require('helmet')
+const app = express();
+app.use(helmet()); //security http headers
+
 const {exec, execSync, execFileSync} = require('child_process');
 const WickrIOAPI = require('wickrio_addon');
 const WickrIOBotAPI = require('wickrio-bot-api');
@@ -15,6 +22,12 @@ logger.level = 'debug';
 var createCsvWriter = require('csv-writer').createObjectCsvWriter;
 var job;
 var verifyUsersMode;
+
+//
+// Web interface definitions
+//
+var bot_port, bot_api_key, bot_api_auth_token;
+var client_auth_codes = {};
 
 process.stdin.resume(); //so the program will not close instantly
 if(!fs.existsSync(process.cwd() + "/attachments")) {
@@ -78,8 +91,330 @@ async function main() {
     }
     bot.setVerificationMode(verifyUsersMode);
 
+    //Passes a callback function that will receive incoming messages into the bot client
+    bot.startListening(listen);
 
-    await bot.startListening(listen); //Passes a callback function that will receive incoming messages into the bot client
+
+    // start up the web interface if one exists
+    if (tokens.BOT_PORT !== undefined &&
+        tokens.BOT_API_KEY != undefined &&
+        tokens.BOT_API_AUTH_TOKEN != undefined) {
+        bot_port = tokens.BOT_PORT.value;
+        bot_api_key = tokens.BOT_API_KEY.value;
+        bot_api_auth_token = tokens.BOT_API_AUTH_TOKEN.value;
+
+        app.listen(bot_port, () => {
+            console.log('We are live on ' + bot_port);
+        });
+
+        // parse application/x-www-form-urlencoded
+        app.use(bodyParser.urlencoded({extended: false}));
+        // parse application/json
+        app.use(bodyParser.json());
+
+        app.use(function(error, req, res, next) {
+            if (error instanceof SyntaxError) {
+                console.log('bodyParser:', error);
+                res.statusCode = 400;
+                res.type('txt').send(error.toString());
+            } else {
+                next();
+            }
+        });
+
+        app.all('*', function(req, res, next) {
+            next();
+        });
+
+        var endpoint = "/WickrIO/V1/Apps/" + bot_api_key;
+
+        app.post(endpoint + "/Authenticate/:wickrUser", function(req, res) {
+            res.set('Content-Type', 'text/plain');
+            res.set('Authorization', 'Basic base64_auth_token');
+            var authHeader = req.get('Authorization');
+            var authToken;
+            if (authHeader) {
+                if (authHeader.indexOf(' ') == -1) {
+                    authToken = authHeader;
+                } else {
+                    authHeader = authHeader.split(' ');
+                    authToken = authHeader[1];
+                }
+            } else {
+                return res.status(401).send('Access denied: invalid Authorization Header format. Correct format: "Authorization: Basic base64_auth_token"');
+            }
+            if (!checkCreds(authToken)) {
+                return res.status(401).send('Access denied: invalid basic-auth token.');
+            } else {
+                var wickrUser = req.params.wickrUser;
+                if (typeof wickrUser !== 'string')
+                    return res.status(400).send('Bad request: WickrUser must be a string.');
+
+                // Check if this user is an administrator
+                var adminUser = bot.myAdmins.getAdmin(wickrUser);
+                if (adminUser === undefined) {
+                    return res.status(401).send('Access denied: ' + wickrUser + ' is not authorized to broadcast!');
+                }
+
+                var ttl = "", bor = "";
+                var users = [];
+                users.push(wickrUser);
+
+                var random = generateRandomString(24);
+                var message = "Authentication code: " + random;
+
+                // Save the auth key for the wickrUser
+                client_auth_codes[wickrUser] = random;
+
+                try {
+                    var csm = WickrIOAPI.cmdSend1to1Message(users, message, ttl, bor);
+                    console.log(csm);
+                    res.send(csm);
+                } catch (err) {
+                    console.log(err);
+                    res.statusCode = 400;
+                    res.send(err.toString());
+                }
+            }
+        });
+
+        app.post(endpoint + "/Broadcast/:wickrUser/:authCode", function(req, res) {
+            res.set('Content-Type', 'text/plain');
+            res.set('Authorization', 'Basic base64_auth_token');
+            var authHeader = req.get('Authorization');
+            var authToken;
+            if (authHeader) {
+                if (authHeader.indexOf(' ') == -1) {
+                    authToken = authHeader;
+                } else {
+                    authHeader = authHeader.split(' ');
+                    authToken = authHeader[1];
+                }
+            } else {
+                return res.status(401).send('Access denied: invalid Authorization Header format. Correct format: "Authorization: Basic base64_auth_token"');
+            }
+
+            if (!checkCreds(authToken)) {
+                return res.status(401).send('Access denied: invalid basic-auth token.');
+            }
+
+            var wickrUser = req.params.wickrUser;
+            if (typeof wickrUser !== 'string')
+                return res.status(401).send("WickrUser must be a string.");
+            var authCode = req.params.authCode;
+            if (typeof authCode !== 'string')
+                return res.status(401).send("Authentication Code must be a string.");
+
+            // Check if the authCode is valid for the input user
+            var dictAuthCode = client_auth_codes[wickrUser];
+            if (dictAuthCode === undefined || authCode != dictAuthCode) {
+                return res.status(401).send('Access denied: invalid user authentication code.');
+            }
+
+//        message.insert("action", "broadcast");
+//    message.insert("message", broadcastTextEdit->toPlainText() );
+//    message.insert("target", 0);
+//    message.insert("acknowledge", broadcastDialogOptionsWidget->isChecked() ? true : false);
+//    message.insert("repeat", 0);
+//    message.insert("pause", 0);
+
+            if (!req.body.message) {
+                return res.send("Broadcast message missing from request.");
+            }
+
+            try {
+                var currentDate = new Date();
+                var jsonDateTime = currentDate.toJSON();
+                var messageID = updateLastID();
+                var broadcastMsgToSend = req.body.message
+                if (req.body.acknowledge != undefined && req.body.acknowledge === true) {
+                  broadcastMsgToSend = broadcastMsgToSend + "\nPlease acknowledge this message by replying with /ack";
+                }
+                broadcastMsgToSend = broadcastMsgToSend + "\n\nBroadcast message sent by: " + wickrUser;
+
+                var bMessage;
+                if (req.body.security_group != undefined) {
+                    if (req.body.security_group.length === 0) {
+                        return res.send("Security Group length invalid.");
+                    }
+                    var securityGroups = [];
+                    securityGroups.push(req.body.security_group);
+                    writeToMessageIdDB(messageID, wickrUser, req.body.security_group, jsonDateTime, req.body.message);
+                    bMessage = WickrIOAPI.cmdSendSecurityGroupMessage(broadcastMsgToSend, securityGroups, "", "", messageID);
+                    console.log('sending to security group ' + req.body.security_group);
+                } else {
+                    writeToMessageIdDB(messageID, wickrUser, "network", jsonDateTime, req.body.message);
+                    bMessage = WickrIOAPI.cmdSendNetworkMessage(broadcastMsgToSend, "", "", messageID);
+                    console.log('sending to entire network!');
+                }
+
+                console.log(bMessage);
+                res.send(bMessage);
+            } catch (err) {
+                console.log(err);
+                res.statusCode = 400;
+                res.send(err.toString());
+            }
+        });
+
+        app.get(endpoint + "/SecGroups/:wickrUser/:authCode", function(req, res) {
+            res.set('Content-Type', 'text/plain');
+            res.set('Authorization', 'Basic base64_auth_token');
+            var authHeader = req.get('Authorization');
+            var authToken;
+            if (authHeader) {
+                if (authHeader.indexOf(' ') == -1) {
+                    authToken = authHeader;
+                } else {
+                    authHeader = authHeader.split(' ');
+                    authToken = authHeader[1];
+                }
+            } else {
+                return res.status(401).send('Access denied: invalid Authorization Header format. Correct format: "Authorization: Basic base64_auth_token"');
+            }
+
+            if (!checkCreds(authToken)) {
+                return res.status(401).send('Access denied: invalid basic-auth token.');
+            }
+
+            var wickrUser = req.params.wickrUser;
+            if (typeof wickrUser !== 'string')
+                return res.status(401).send("WickrUser must be a string.");
+            var authCode = req.params.authCode;
+            if (typeof authCode !== 'string')
+                return res.status(401).send("Authentication Code must be a string.");
+
+            // Check if the authCode is valid for the input user
+            var dictAuthCode = client_auth_codes[wickrUser];
+            if (dictAuthCode === undefined || authCode != dictAuthCode) {
+                return res.status(401).send('Access denied: invalid user authentication code.');
+            }
+
+            try {
+              var getGroups = WickrIOAPI.cmdGetSecurityGroups();
+              var response = isJson(getGroups);
+              if (response !== false) {
+                getGroups = response;
+              } else {
+                getGroups = '{}';
+              }
+              res.set('Content-Type', 'application/json');
+              res.send(getGroups);
+              console.log(getGroups);
+            } catch (err) {
+              console.log(err);
+              res.statusCode = 400;
+              res.type('txt').send(err.toString());
+            }
+        });
+
+        app.get(endpoint + "/Status/:wickrUser/:authCode", function(req, res) {
+            res.set('Content-Type', 'text/plain');
+            res.set('Authorization', 'Basic base64_auth_token');
+            var authHeader = req.get('Authorization');
+            var authToken;
+            if (authHeader) {
+                if (authHeader.indexOf(' ') == -1) {
+                    authToken = authHeader;
+                } else {
+                    authHeader = authHeader.split(' ');
+                    authToken = authHeader[1];
+                }
+            } else {
+                return res.status(401).send('Access denied: invalid Authorization Header format. Correct format: "Authorization: Basic base64_auth_token"');
+            }
+
+            if (!checkCreds(authToken)) {
+                return res.status(401).send('Access denied: invalid basic-auth token.');
+            }
+
+            var wickrUser = req.params.wickrUser;
+            if (typeof wickrUser !== 'string')
+                return res.status(401).send("WickrUser must be a string.");
+            var authCode = req.params.authCode;
+            if (typeof authCode !== 'string')
+                return res.status(401).send("Authentication Code must be a string.");
+
+            // Check if the authCode is valid for the input user
+            var dictAuthCode = client_auth_codes[wickrUser];
+            if (dictAuthCode === undefined || authCode != dictAuthCode) {
+                return res.status(401).send('Access denied: invalid user authentication code.');
+            }
+
+
+            var messageIdEntries = getMessageEntries(wickrUser, 20);
+            var reply = "";
+            if (messageIdEntries.length < 1){
+                reply = strings["noPrevious"];
+            } else {
+                var length = Math.min(messageIdEntries.length, 5);
+                var contentData;
+                var index = 1;
+                var messageList = [];
+                var messageString = "";
+                for (var i = 0; i < messageIdEntries.length; i++){
+                    contentData = WickrIOAPI.cmdGetMessageIDEntry(messageIdEntries[i].message_id);
+                    var contentParsed = JSON.parse(contentData);
+                    messageIdEntries[i]['message'] = contentParsed.message;
+//                    messageList.push(contentParsed.message);
+//                    messageString += '(' + index++ + ') ' + contentParsed.message + "\n";
+                }
+//                reply = strings["whichMessage"].replace("%{length}", length).replace("%{messageList}", messageString);
+                reply = JSON.stringify(messageIdEntries);
+            }
+            res.set('Content-Type', 'application/json');
+            return res.send(reply);
+        });
+
+        app.get(endpoint + "/Status/:wickrUser/:authCode/:messageID", function(req, res) {
+            res.set('Content-Type', 'text/plain');
+            res.set('Authorization', 'Basic base64_auth_token');
+            var authHeader = req.get('Authorization');
+            var authToken;
+            if (authHeader) {
+                if (authHeader.indexOf(' ') == -1) {
+                    authToken = authHeader;
+                } else {
+                    authHeader = authHeader.split(' ');
+                    authToken = authHeader[1];
+                }
+            } else {
+                return res.status(401).send('Access denied: invalid Authorization Header format. Correct format: "Authorization: Basic base64_auth_token"');
+            }
+
+            if (!checkCreds(authToken)) {
+                return res.status(401).send('Access denied: invalid basic-auth token.');
+            }
+
+            var wickrUser = req.params.wickrUser;
+            if (typeof wickrUser !== 'string')
+                return res.status(401).send("WickrUser must be a string.");
+            var authCode = req.params.authCode;
+            if (typeof authCode !== 'string')
+                return res.status(401).send("Authentication Code must be a string.");
+
+            // Check if the authCode is valid for the input user
+            var dictAuthCode = client_auth_codes[wickrUser];
+            if (dictAuthCode === undefined || authCode != dictAuthCode) {
+                return res.status(401).send('Access denied: invalid user authentication code.');
+            }
+
+
+            var statusData = WickrIOAPI.cmdGetMessageStatus(req.params.messageID, "summary", "0", "1000");
+            var reply = statusData;
+            res.set('Content-Type', 'application/json');
+            return res.send(reply);
+        });
+
+        // What to do for ALL requests for ALL Paths
+        // that are not handled above
+        app.all('*', function(req, res) {
+            console.log('*** 404 ***');
+            console.log('404 for url: ' + req.url);
+            console.log('***********');
+            return res.type('txt').status(404).send('Endpoint ' + req.url + ' not found');
+        });
+    }
   } catch (err) {
     console.log(err);
   }
@@ -189,7 +524,7 @@ function listen(message) {
       //check argument here!
       //args = argument.split(' ');
       if (argument === ''){
-        var messageIdEntries = getMessageEntries(userEmail);
+        var messageIdEntries = getMessageEntries(userEmail, 5);
         var reply = "";
         if(messageIdEntries.length < 1){
           reply = strings["noPrevious"];
@@ -222,7 +557,7 @@ function listen(message) {
     }
 
     if (command === '/report') {
-      var messageIdEntries = getMessageEntries(userEmail);
+      var messageIdEntries = getMessageEntries(userEmail, 5);
       var reply = "";
       if(messageIdEntries.length < 1){
         reply = strings["noPrevious"];
@@ -492,7 +827,7 @@ function listen(message) {
     } else if (user.confirm === 'askMessageId' && fullMessage != "/status") {
       //Subtract one to account for 0 based indexes
       var index = parseInt(fullMessage) - 1;
-      var messageIdEntries = getMessageEntries(userEmail);
+      var messageIdEntries = getMessageEntries(userEmail, 5);
       var length = Math.min(messageIdEntries.length, 5);
       var reply = "";
       if(isNaN(index) || index < 0 || index >= length){
@@ -509,7 +844,7 @@ function listen(message) {
       var index = parseInt(fullMessage) - 1;
       var reply = "";
       //TODO make this a global~?
-      var messageIdEntries = getMessageEntries(userEmail);
+      var messageIdEntries = getMessageEntries(userEmail, 5);
       var length = Math.min(messageIdEntries.length, 5);
       if(isNaN(index) || index < 0 || index >= length){
         user.confirm = 'idForReport';
@@ -759,7 +1094,7 @@ function repeatMessage(broadcast, user, vGroupID, messageID, userEmail, target) 
   cronJob(job, cronInterval, user, broadcast, securityGroupFlag, askForAckFlag, securityGroupsToSend, userEmail, target);
 }
 
-function getMessageEntries(userEmail){
+function getMessageEntries(userEmail, max){
   var messageIdEntries = []
   var tableDataRaw = WickrIOAPI.cmdGetMessageIDTable("0","1000");
   var tableData = JSON.parse(tableDataRaw);
@@ -770,7 +1105,7 @@ function getMessageEntries(userEmail){
     if (entry.sender === userEmail) {
       messageIdEntries.push(entry);
     }
-    if (messageIdEntries.length > 4) {
+    if (messageIdEntries.length >= max) {
       break;
     }
   }
@@ -912,6 +1247,38 @@ function writeCSVReport(path, csvArray) {
     .then(() => {
       logger.debug('...Done');
     });
+}
+
+//Basic function to validate credentials for example
+function checkCreds(authToken) {
+    try {
+        var valid = true;
+        const authStr = Buffer.from(authToken, 'base64').toString();
+        //implement authToken verification in here
+        if (authStr !== bot_api_auth_token)
+            valid = false;
+        return valid;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function generateRandomString(length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < length; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function isJson(str) {
+    try {
+      str = JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return str;
 }
 
 main();
--- configure.js
+++ configure.js
@@ -1,4 +1,6 @@
 const WickrIOBotAPI = require('wickrio-bot-api');
+const util = require('util')
+
 
 require("dotenv").config({
   path: `.env.configure`
@@ -39,10 +41,82 @@ process.on('uncaughtException', exitHandler.bind(null, {
 
 main();
 
+
 async function main()
 {
+    const tokenConfig = [
+        {
+            token: 'WEB_INTERFACE',
+            pattern: 'yes|no',
+            type: 'string',
+            description: 'Do you want to setup the web interface',
+            message: 'Please enter either yes or no',
+            required: true,
+            default: 'no',
+            list: [
+                {
+                    token: 'LISTEN_PORT',
+                    pattern: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
+                    type: 'number',
+                    description: "Please enter your client bot's port",
+                    message: 'Cannot leave empty! Please enter a value',
+                    required: false,
+                    default: 'N/A',
+                },
+                {
+                    token: 'API_KEY',
+                    pattern: '',
+                    type: 'string',
+                    description: "Please enter your client bot's API-Key",
+                    message: 'Cannot leave empty! Please enter a value',
+                    required: true,
+                    default: 'N/A',
+                },
+                {
+                    token: 'API_AUTH_TOKEN',
+                    pattern: '',
+                    type: 'string',
+                    description: 'Please create an Web API Basic Authorization Token, we recommend an alphanumeric string with at least 24 characters',
+                    message: 'Cannot leave empty! Please enter a value',
+                    required: true,
+                    default: 'N/A',
+                },
+                {
+                    token: 'HTTPS_CHOICE',
+                    pattern: 'yes|no',
+                    type: 'string',
+                    description: 'Do you want to set up an HTTPS connection with the Web API Interface, highly recommended',
+                    message: 'Please enter either yes or no',
+                    required: true,
+                    default: 'no',
+                    list: [
+                        {
+                            token: 'SSL_KEY_LOCATION',
+                            pattern: '',
+                            type: 'file',
+                            description: 'Please enter the name and location of your SSL .key file',
+                            message: 'Cannot fine file!',
+                            required: true,
+                            default: 'N/A',
+                        },
+                        {
+                            token: 'SSL_CRT_LOCATION',
+                            pattern: '',
+                            type: 'file',
+                            description: 'Please enter the name and location of your SSL .crt file',
+                            message: 'Cannot fine file!',
+                            required: true,
+                            default: 'N/A',
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+
+
     var fullName = process.cwd() + "/processes.json";
-    wickrIOConfigure = new WickrIOBotAPI.WickrIOConfigure([], fullName, true, true);
+    wickrIOConfigure = new WickrIOBotAPI.WickrIOConfigure(tokenConfig, fullName, true, true);
 
     await wickrIOConfigure.configureYourBot("WickrIO-Broadcast-Bot");
     process.exit();
