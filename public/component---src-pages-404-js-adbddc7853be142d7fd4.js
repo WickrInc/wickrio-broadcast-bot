85325f7b00558d1f860c7ade130bbf52a12db755
alex@aml.codes <alex@aml.codes>
2020-06-12 12:00:45 -0400

    v.8 of web

---
+++ public/component---src-pages-404-js-adbddc7853be142d7fd4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("I/Ru");t.default=function(e){e.location;return u.a.createElement(l.a,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-adbddc7853be142d7fd4.js.map
---
+++ public/component---src-pages-page-2-tsx-c6b4451b41e90d401d63.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"+xhR":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("I/Ru"),r=a("vrFN");t.default=function(e){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2 (",e.path,")"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-c6b4451b41e90d401d63.js.map
