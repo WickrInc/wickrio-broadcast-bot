277b7ae9dfeaefee95db46ce40c5179e7d36a03a
alex@aml.codes <alex@aml.codes>
2020-06-18 13:15:59 -0400

    add tests and register jest with babel

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // if you're also using typescript
+  testEnvironment: "node",
+  verbose: true,
+  // registers babel.config.js with jest
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  // explicitly include any node libs using ESM modules
+  // transformIgnorePatterns: ["node_modules/?!(<ESM module here>|<another here>|<etc...>)"],
+}
