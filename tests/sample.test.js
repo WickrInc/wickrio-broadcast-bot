d4eaa6a0474a6f94dfa7fc585cf3d70496984f40
alex@aml.codes <alex@aml.codes>
2020-07-21 18:44:27 +0000

    test

--- tests/sample.test.js
+++
@@ -1,8 +0,0 @@
-// import request from 'supertest'
-// import app from '../server'
-
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true)
-  })
-})
