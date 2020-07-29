35cbcaf2949eb02a3dfc4502244695beaf4e734d
pwcushman <pcushman@wickr.com>
2020-07-29 14:29:39 -0700

    merge changes from master

--- src/services/status-service.js
+++ src/services/status-service.js
@@ -26,7 +26,7 @@ class StatusService {
       for (const userReply of messageStatus) {
         // const userReply = messageStatus[i]
         console.log({ userReply })
-        if (userReply.status_message) {
+        if ((userReply.status === 3 || userReply.status === 6) && userReply.status_message) {
           console.log('located a user')
           locatedusers = true
           const { location } = JSON.parse(userReply.status_message)
