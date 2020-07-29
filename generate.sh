e8ba5221ce08ef46f5ac583850d8c48ca9179f62
pwcushman <pcushman@wickr.com>
2020-07-29 13:36:19 -0700

    Remove the tar of .babelrc

--- generate.sh
+++ generate.sh
@@ -11,6 +11,6 @@ mkdir -p temp
 cp -r src public configure.js upgrade.js configure.sh install.sh package.json processes.json README.md restart.sh start.sh stop.sh upgrade.sh temp
 
 cd temp
-tar czf $1/software.tar.gz * .babelrc
+tar czf $1/software.tar.gz *
 cd ..
 rm -r temp
