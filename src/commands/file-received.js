af3c57e95f15b71f6b57d93f6024360b2d95fc78
toren-wickr <tderosa@wickr.com>
2020-11-10 23:07:05 -0500

    Fixing send only mode for files

--- src/commands/file-received.js
+++ src/commands/file-received.js
@@ -1,6 +1,6 @@
 import State from '../state'
 
-import BROADCAST_ENABLED from '../helpers/constants'
+import { BROADCAST_ENABLED } from '../helpers/constants'
 // TODO add a delete file command??
 class FileReceived {
   constructor({ fileService, messageService }) {
@@ -25,8 +25,12 @@ class FileReceived {
     // this.fileService.setFile(file)
     this.fileService.setFilePath(filePath)
     this.fileService.setFilename(fileName)
-    const broadcastLetterString = BROADCAST_ENABLED ? '(b)roadcast, ' : ''
-    const broadcastString = BROADCAST_ENABLED ? 'broadcast this file or ' : ''
+    console.log('BROADCAST_ENABLED' + BROADCAST_ENABLED)
+    console.log('BROADCAST_ENABLED value' + BROADCAST_ENABLED.value)
+    const broadcastLetterString =
+      BROADCAST_ENABLED?.value === 'no' ? '' : '(b)roadcast, '
+    const broadcastString =
+      BROADCAST_ENABLED.value === 'no' ? '' : 'broadcast this file or '
     const reply = `Would you like to ${broadcastString}send this file to a list? Or is it a file of usernames or hashes? Please respond with ${broadcastLetterString}(s)end, (u)ser, or (h)ash`
     const obj = {
       reply,
--- src/commands/help.js
+++ src/commands/help.js
@@ -27,6 +27,10 @@ class Help {
 
   execute() {
     const { isAdmin, vGroupID } = this.messageService
+    console.log('WEB_APP: ' + WEB_APPLICATION)
+    console.log('MAP_ENABLED: ' + BOT_MAPS)
+    console.log('BROADCAST_ENABLED: ' + BROADCAST_ENABLED)
+    console.log('BROADCAST_ENABLED.value: ' + BROADCAST_ENABLED.value)
 
     let webAppString = ''
     let broadcastString = ''
@@ -41,29 +45,18 @@ class Help {
         '*Map Commands*\n' +
         '/map : Get a picture of everybody who responded to a broadcast with their location\n\n'
     }
-    if (!BROADCAST_ENABLED || BROADCAST_ENABLED.value === 'yes') {
+    if (BROADCAST_ENABLED?.value === 'no') {
+      if (webAppEnabled) {
+        webAppString =
+          '*Web App Commands*\n' +
+          '/panel: is currently disabled as it only supports broadcasts which are currently disabled\n\n'
+      }
+      broadcastString = '/broadcast: is currently disabled\n'
+    } else {
       broadcastString =
         '/broadcast <Message> : To send a broadcast message to the network or security groups\n' +
         'To broadcast a file - Click the + sign and share the file with the bot\n' +
         'To broadcast a voice memo - Click the microphone button and send a voice memo to the bot\n'
-    } else if (BROADCAST_ENABLED.value === 'no') {
-      broadcastString = '/broadcast is currently disabled\n'
-      if (webAppEnabled) {
-        webAppString =
-          '*Web App Commands*\n' +
-          '/panel is currently disabled as it only supports broadcasts which are currently disabled\n\n'
-      }
-      broadcastString =
-        '/broadcast <Message> : To send a broadcast message to the network or security groups\n' +
-        'To broadcast a file - Click the + sign and share the file with the bot\n' +
-        'To broadcast a voice memo - Click the microphone button and send a voice memo to the bot\n'
-    } else if (BROADCAST_ENABLED.value === 'no') {
-      broadcastString = '/broadcast is currently disabled\n'
-      if (webAppEnabled) {
-        webAppString =
-          '*Web App Commands*\n' +
-          '/panel is currently disabled as it only supports broadcasts which are currently disabled\n\n'
-      }
     }
     let helpString =
       '*Messages Commands*\n' +
@@ -83,7 +76,7 @@ class Help {
       '/version : Get the version of the integration\n' +
       '/cancel : To cancel the last operation and enter a new command\n' +
       '/files : To get a list of saved files available for the /send command\n' +
-      '/delete : To delete a file that was previously made available for the /send command\n'
+      '/delete : To delete a file that was previously made available for the /send command'
 
     if (isAdmin) {
       // console.log({ helpString, vGroupID })
