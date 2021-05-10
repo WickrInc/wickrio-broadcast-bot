d3ce01ae80fe045bd0457b7c367d5bd19588ed8d
pwcushman <pcushman@wickr.com>
2021-05-09 20:38:48 -0400

    use list for the /map command

--- src/commands/map.js
+++ src/commands/map.js
@@ -19,6 +19,8 @@ class Map {
   execute() {
     let reply
     let state
+    let messagemeta = {}
+
     if (mapEnabled === true) {
       const { userEmail } = this.messageService
       this.genericService.resetIndexes()
@@ -27,22 +29,60 @@ class Map {
         userEmail,
         false
       )
+
       if (!entries || entries.length === 0) {
         reply = entriesString
       } else {
         reply = `${entriesString}Which message would you like to see the map of?`
+
+        messagemeta = {
+          table: {
+            name: 'List of Sent Broadcasts',
+            firstcolname: 'Message',
+            actioncolname: 'Select',
+            rows: [],
+          },
+          textcut: [
+            {
+              startindex: 0,
+              endindex: entriesString.length - 1,
+            },
+          ],
+        }
+        for (let i = 0; i < entries.length; i++) {
+          const response = i + 1
+          const row = {
+            firstcolvalue: entries[i].message,
+            response: response.toString(),
+          }
+          messagemeta.table.rows.push(row)
+        }
       }
+
+      // If the number of messages is greater than the end index
+      // then let the user know about the "more" command
       if (entries.length > this.genericService.getEndIndex()) {
+        const startindex = reply.length
+
         reply += '\nOr to see more messages reply more'
+
+        const tcrow = {
+          startindex: startindex,
+          endindex: reply.length,
+        }
+        messagemeta.textcut.push(tcrow)
       }
+
       state = State.WHICH_MAP
     } else {
       reply = 'Map is disabled, configure it with our google maps API key.'
       state = State.NONE
     }
+
     return {
       reply,
       state,
+      messagemeta,
     }
   }
 }
