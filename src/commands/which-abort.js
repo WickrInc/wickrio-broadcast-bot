af64069d7699b4e09b197e633ba509a9e57e3704
toren-wickr <tderosa@wickr.com>
2021-10-07 13:28:22 -0400

    Updating index out of range for report, abort, and status

--- src/commands/which-abort.js
+++ src/commands/which-abort.js
@@ -17,27 +17,24 @@ class WhichAbort {
     let reply
     let state
     const userEmail = this.messageService.userEmail
-    const currentEntries = this.genericService.getMessageEntries(
-      userEmail,
-      true
-    )
+    const entries = this.genericService.getMessageEntries(userEmail, true)
     const index = this.messageService.message
     if (index === 'more') {
       this.genericService.incrementIndexes()
       reply = this.genericService.getEntriesString(userEmail, true)
-      if (currentEntries.length > this.genericService.getEndIndex()) {
+      if (entries.length > this.genericService.getEndIndex()) {
         reply += 'Or to see more messages reply more'
       }
       state = this.state
     } else if (
       !this.messageService.isInt() ||
       index < 1 ||
-      index > currentEntries.length
+      index > entries.length
     ) {
-      reply = `Index: ${index} is out of range. Please enter a whole number between 1 and ${currentEntries.length} or type /cancel to end previous flow.`
+      reply = `Index: ${index} is out of range. Please enter a whole number between 1 and ${entries.length} or type /cancel to end previous flow.`
       state = this.state
     } else {
-      const messageID = `${currentEntries[parseInt(index, 10) - 1].message_id}`
+      const messageID = `${entries[parseInt(index, 10) - 1].message_id}`
       reply = this.genericService.cancelMessageID(messageID)
       state = State.NONE
     }
