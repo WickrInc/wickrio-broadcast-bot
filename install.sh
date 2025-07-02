35433eaf3a1433bb74541cf604bca81e7f6d7ce5
Xiaoman Yang <xmy@amazon.com>
2025-07-02 15:27:02 -0700

    change back to broadbast integration(previously used custom integration for testing)

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+!/bin/sh
 
-# comment out for testing
-# if [ -f "/usr/local/nvm/nvm.sh" ]; then
-#   . /usr/local/nvm/nvm.sh
-# fi
+comment out for testing
+if [ -f "/usr/local/nvm/nvm.sh" ]; then
+  . /usr/local/nvm/nvm.sh
+fi
 
-# if [ -f ./node_modules.tgz ]
-# then
-#     tar -xvf ./node_modules.tgz
-# else
-#     npm install --unsafe-perm
-# fi
+if [ -f ./node_modules.tgz ]
+then
+    tar -xvf ./node_modules.tgz
+else
+    npm install --unsafe-perm
+fi
 
-# npm run build
-exit 0
+npm run build
