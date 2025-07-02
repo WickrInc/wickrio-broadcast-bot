be9ab030d98f814e43ae4d2579662b37d58736ae
Xiaoman Yang <xmy@amazon.com>
2025-07-02 15:27:02 -0700

    change back to broadbast integration(previously used custom integration for testing)

--- start.sh
+++ start.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-# if [ -f "/usr/local/nvm/nvm.sh" ]; then
-#   . /usr/local/nvm/nvm.sh
-# fi
-
-# npm start
-# Load NVM if available
+!/bin/bash
 if [ -f "/usr/local/nvm/nvm.sh" ]; then
   . /usr/local/nvm/nvm.sh
 fi
 
-# Start the broadcast bot using the built index.js
-node ./build/index.js "$@"
+npm start
